3ed16f1889a0954d773ea2460e9eb509f1838b1d
Alen Demirov <52421176+aalend@users.noreply.github.com>
2023-07-27 12:06:28 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import isOibValid from './oib-validator';
+
+const oibInput = document.querySelector('input');
+const checkOib = document.querySelector('form');
+
+checkOib.addEventListener('submit', e => {
+	e.preventDefault();
+
+	isOibValid(oibInput.value);
+});
---
+++ oib-validator.js
@@ -0,0 +1,45 @@
+const valid = document.querySelector('.valid');
+const invalid = document.querySelector('.invalid');
+const oibError = document.querySelector('.error');
+
+export default function isOibValid(input) {
+	const oib = input.toString();
+
+	if (oib.match(/\d{11}/) === null || oib.match(/\d{11}/ > 11)) {
+		valid.classList.remove('visible');
+		invalid.classList.remove('visible');
+		oibError.classList.add('visible');
+		return false;
+	}
+
+	let calculated = 10;
+	for (const digit of oib.substring(0, 10)) {
+		calculated += parseInt(digit);
+
+		calculated %= 10;
+		if (calculated === 0) {
+			calculated = 10;
+		}
+
+		calculated *= 2;
+		calculated %= 11;
+	}
+
+	const check = 11 - calculated;
+
+	if (check === 10) {
+		check = 0;
+	}
+
+	if (check === Number(oib.slice(10))) {
+		oibError.classList.remove('visible');
+		invalid.classList.remove('visible');
+		valid.classList.add('visible');
+	} else {
+		oibError.classList.remove('visible');
+		valid.classList.remove('visible');
+		invalid.classList.add('visible');
+	}
+
+	return check === parseInt(oib[10]);
+}
